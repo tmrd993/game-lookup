4451039e871cf47a6a33f9bf2abd0315a427e825
tmrd993 <tmerd@hotmail.de>
2021-05-08 11:46:16 +0200

    create initial project structure

---
+++ src/main/java/com/timucin/gamelookup/GameLookupApplication.java
@@ -0,0 +1,13 @@
+package com.timucin.gamelookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timucin/gamelookup/GameLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timucin.gamelookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
