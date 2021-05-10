56483b78525789df93aab845366cb35746a6fca7
tmrd993 <tmerd@hotmail.de>
2021-05-10 11:12:38 +0200

    add service annotation to service classes

--- src/main/java/com/timucin/gamelookup/service/GameService.java
+++ src/main/java/com/timucin/gamelookup/service/GameService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.timucin.gamelookup.domain.Game;
 import com.timucin.gamelookup.repository.GameRepository;
 
+@Service
 public class GameService {
 	
 	private final GameRepository gameRepository;
--- src/main/java/com/timucin/gamelookup/service/GenreService.java
+++ src/main/java/com/timucin/gamelookup/service/GenreService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.timucin.gamelookup.domain.Genre;
 import com.timucin.gamelookup.repository.GenreRepository;
 
+@Service
 public class GenreService {
 	
 private final GenreRepository genreRepository;
