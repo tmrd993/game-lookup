ffdfd101785a2d1ac163588663467fbb1859884c
tmrd993 <tmerd@hotmail.de>
2021-05-12 12:27:19 +0200

    add post handling for web search controller and results view

--- src/main/java/com/timucin/gamelookup/controller/WebSearchController.java
+++ src/main/java/com/timucin/gamelookup/controller/WebSearchController.java
@@ -1,15 +1,48 @@
 package com.timucin.gamelookup.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.timucin.gamelookup.domain.Game;
+import com.timucin.gamelookup.fetcher.GameDataFetcher;
 
 @Controller
 public class WebSearchController {
 	
+	Logger logger = LoggerFactory.getLogger(WebSearchController.class);
+	
+	private final GameDataFetcher gameDataFetcher;
+	
+	@Autowired
+	public WebSearchController(GameDataFetcher gameDataFetcher) {
+		this.gameDataFetcher = gameDataFetcher;
+	}
 	
 	@GetMapping("/web_search")
 	public String webSearch() {
 		return "web_search";
+	}
+	
+	@PostMapping("/web_search")
+	public String results(@RequestParam(required = true, value = "query") String query, Model model) {
+		
+		logger.info("Search query: " + query);
+		
+		List<Game> searchResults = gameDataFetcher.fetchAll(query);
+		
+		logger.info("Found " + searchResults.size() + " results.");
+		
+		model.addAttribute("searchResults", searchResults);
+		
+		return "search_results";
 	}
 
 }
--- src/main/java/com/timucin/gamelookup/fetcher/RawgGameDataFetcher.java
+++ src/main/java/com/timucin/gamelookup/fetcher/RawgGameDataFetcher.java
@@ -1,12 +1,23 @@
 package com.timucin.gamelookup.fetcher;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriUtils;
 
 import com.timucin.gamelookup.domain.Game;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 /**
  * 
@@ -16,13 +27,36 @@ import com.timucin.gamelookup.domain.Game;
 @Primary
 public class RawgGameDataFetcher implements GameDataFetcher {
 	
+	Logger logger = LoggerFactory.getLogger(RawgGameDataFetcher.class);
+	
 	@Value("${game.db.rawg.url}")
 	private String rawgRequestUrl;
 
 	@Override
-	public List<Game> fetchAll(String query) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Game> fetchAll(@NonNull String query) {
+		
+		OkHttpClient client = new OkHttpClient();
+		
+		String fullRequestUrl = rawgRequestUrl + UriUtils.encodeQuery(query, StandardCharsets.UTF_8);
+		
+		Request request = new Request.Builder()
+				.url(fullRequestUrl)
+				.build();
+		
+		List<Game> queryResults = new ArrayList<>();
+		
+		try {
+			ResponseBody responseBody = client.newCall(request).execute().body();
+			
+			logger.info(responseBody.toString());
+			
+			//TODO: implement json deserializer and convert response to list of games
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return queryResults;
 	}
 
 }
