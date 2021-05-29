b680055aa0091735ffb42b9331387d5c6ac813c9
tmrd993 <tmerd@hotmail.de>
2021-05-29 12:17:08 +0200

    add spring security and basic configuration

--- src/main/java/com/timucin/gamelookup/domain/User.java
+++ src/main/java/com/timucin/gamelookup/domain/User.java
@@ -1,9 +1,16 @@
 package com.timucin.gamelookup.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	
 	private String username;
 	
@@ -11,6 +18,14 @@ public class User {
 	private String password;
 	
 	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getUsername() {
 		return username;
